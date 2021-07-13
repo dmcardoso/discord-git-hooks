98368364b037383dbf4aa040155644530581912a
Daniel Moreira <dmcardoso.99@hotmail.com>
2021-07-13 17:29:33 -0300

    feat: add express and webhooks endpoints

--- src/server.js
+++ src/server.js
@@ -1,4 +1,34 @@
 require('dotenv').config();
+const express = require('express');
+
+const app = express();
+
+app.get('/', (request, response) => {
+  return response.json({
+    message: 'Hello world!',
+  });
+});
+
+app.post('/branchs', (request, response) => {
+  return response.send('branchs');
+});
+
+app.post('/forks', (request, response) => {
+  return response.send('forks');
+});
+
+app.post('/pull-requests', (request, response) => {
+  return response.send('pull-requests');
+});
+
+app.post('/stars', (request, response) => {
+  return response.send('stars');
+});
+
+app.listen(process.env.APP_PORT, () => {
+  console.log(`I'm on port ${process.env.APP_PORT}`);
+});
+
 const Discord = require('discord.js');
 
 const client = new Discord.Client();

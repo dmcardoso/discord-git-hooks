590b537db800d234d7e45b87d8d3098a6d656205
Daniel Moreira <dmcardoso.99@hotmail.com>
2021-07-14 15:01:28 -0300

    feat: add stars endpoint and trigger message

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 require('dotenv').config();
 const express = require('express');
+const Discord = require('discord.js');
 
 const app = express();
+
+const client = new Discord.Client();
+
+app.use(express.json());
 
 app.get('/', (request, response) => {
   return response.json({
@@ -21,20 +26,52 @@ app.post('/pull-requests', (request, response) => {
   return response.send('pull-requests');
 });
 
-app.post('/stars', (request, response) => {
-  return response.send('stars');
+app.post('/stars', async (request, response) => {
+  const {
+    body: { action, repository, sender, starred_at: starredAt },
+  } = request;
+
+  if (action === 'created') {
+    const embed = new Discord.MessageEmbed()
+      .setTitle(`${sender.login} starred ${repository.name}!`)
+      .setDescription(
+        `Check ${sender.login} and start following! ${sender.html_url}`,
+      )
+      .setURL(repository.html_url)
+      .setFooter(repository.full_name)
+      .setThumbnail(sender.avatar_url)
+      .setColor('RANDOM')
+      .addFields([
+        {
+          name: 'Repository description',
+          value: repository.description,
+        },
+        {
+          name: 'Repository stars count',
+          value: repository.stargazers_count,
+        },
+        {
+          name: 'Repository language',
+          value: repository.language,
+        },
+        {
+          name: 'Repository open issues',
+          value: repository.open_issues,
+        },
+      ])
+      .setTimestamp(new Date(starredAt).getTime());
+
+    const channel = await client.channels.fetch(process.env.CHANNEL_ID);
+    await channel.send('@everyone 🎉 🎉 new star!! 🎉 🎉', embed);
+  }
+  return response.status(204);
 });
 
 app.listen(process.env.APP_PORT, () => {
   console.log(`I'm on port ${process.env.APP_PORT}`);
+  client.login(process.env.DISCORD_TOKEN);
 });
-
-const Discord = require('discord.js');
-
-const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log('I am ready!');
 });
-
-client.login(process.env.DISCORD_TOKEN);
